81ca89deb0c26f649d865b122ea0a903a06de3b6
MogwaiMomo <momokoprice@gmail.com>
2014-06-17 16:52:02 -0700

    Added music onclick using HTML5 Audio API.

--- js/flow.js
+++ js/flow.js
@@ -62,10 +62,74 @@
 			ctx.fillRect(0, 0, canvas.width, boxHeight);
 			boxHeight += pixelsPerFrame;
 		} 
+		
 	  }
 
+// Animation stuff ends here // 
 
-	// var page = document.getElementById('page');
+
+
+// Sound stuff starts here: //
+
+var context;
+var url ="data/07 In The Lost and Found (Honky Bach).mp3";
+
+//executes on page click:
+function init() {
+	try {
+		// fix up for prefixing
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		context = new AudioContext();
+		loadSound(context, url);
+		}
+	// catch non-supported browsers
+	catch(error) {
+		alert(error)
+	}
+};
+
+function loadSound(context, url) {
+
+	// Creating the XHTTP object
+	var request = new XMLHttpRequest();
+
+	// Open this URL 
+	request.open('GET', url, true);
+
+	// This is what format is going to be 
+	request.responseType = 'arraybuffer';
+
+	// .onload is an event which fires when the request data has fully loaded
+	request.onload = function() {
+		// create the sound source:
+		soundSource = context.createBufferSource();
+
+		// import callback function that provides decoded PCM audio data 
+		context.decodeAudioData(request.response, 
+			function(buffer) {
+				//transfer decoded audio data ("buffer") to sound source object
+				soundSource.buffer = buffer;
+
+				//connect to speakers
+				soundSource.connect(context.destination);
+				soundSource.start(0); 
+			},
+			//catch errors 
+			function(e) {
+				console.log(e)
+			}
+		);
+	}
+
+	request.send();
+};
+
+
+
+
+
+// Trigger events: 
+
 
 	//Fade out the intro
 	window.addEventListener('click', function(ev){
@@ -78,5 +142,10 @@
 		ev.preventDefault();
 		requestAnimationFrame(animate);
 	}, false);
+
+	window.addEventListener("click", function(ev){
+		ev.preventDefault();
+		init();
+	}, false); 
 		
 	}());
